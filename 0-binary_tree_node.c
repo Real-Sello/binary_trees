ed75cfbc9609686742a40e55ac114873268f6946
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-04-08 21:48:45 +0200

    Indentation fixes

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_node - a function that creates a binary tree node
-* @parent: pointer to the parent node of the node to create
-* @value: value to put in the new node
-* Return: pointer to the new node or NULL on failure
-*/
+ * binary_tree_node - a function that creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * Return: pointer to the new node or NULL on failure
+ */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
@@ -14,10 +14,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (!new_node)
 		return (NULL);
 
-		new_node->parent = parent;
-		new_node->left = NULL;
-		new_node->right = NULL;
-		new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
 
-		return (new_node);
+	return (new_node);
 }
