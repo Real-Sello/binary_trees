d21a3795cec9d963535f2f1473651787a08fa380
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-04-09 12:22:02 +0200

    Add leaf node check to tree traversal algorithm

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,19 +9,23 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height, right_height;
+size_t left_height = 0, right_height = 0;
 if (tree == NULL)
 {
 return (0);
 }
+if (!tree->left && !tree->right)
+return (0);
 left_height = binary_tree_height(tree->left);
+left_height++;
 right_height = binary_tree_height(tree->right);
-if (left_height > right_height)
+right_height++;
+if (left_height < right_height)
 {
-return (left_height + 1);
+return (left_height);
 }
 else
 {
-return (right_height + 1);
+return (right_height);
 }
 }
