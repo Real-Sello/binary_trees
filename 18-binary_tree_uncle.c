fdfe5425cfc5180da5249ac0e13fc6440d085460
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-04-09 20:06:09 +0200

    18. Uncle

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - function that finds the uncle of a node
+ * @node: pointer to the node to find the uncle
+ * Return: - pointer to the uncle node
+ *         - if node is NULL, return NULL
+ *         - if node has no uncle, return NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+return (NULL);
+
+binary_tree_t *grandparent = node->parent->parent;
+
+if (grandparent->left == NULL || grandparent->right == NULL)
+return (NULL);
+
+if (grandparent->left == node->parent)
+return (grandparent->right);
+
+return (grandparent->left);
+}
