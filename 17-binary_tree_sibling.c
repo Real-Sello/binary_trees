e0f1b1407c3ba58e5bce61cec9971c3dc4ef6582
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-04-09 19:47:57 +0200

    17. Sibling|Refactor

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,10 +10,11 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+binary_tree_t *sibling;
+
 if (node == NULL || node->parent == NULL)
 return (NULL);
 
-binary_tree_t *sibling;
 if (node->parent->left == node)
 sibling = node->parent->right;
 else
